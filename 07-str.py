fa5b181b076802d402d71eb294afd60746a1f575
Desperado-Tolive <505263467@qq.com>
2018-08-29 08:37:53 +0800

    finish 带笔记，运行结果，对find、index说明进行补充 有附加资料

--- 07-str.py
+++ 07-str.py
@@ -88,12 +88,12 @@ print(len(s1))  # 22
 print(s.count("m",7))   # 1
 print()
 
-#8 str.find()   查找字符串中是否具有指定的字符串，返回第一次出现的位置，查找不到返回-1
+#8 str.find()   查找字符串中是否具有指定的字符串，返回第一次出现的位置（下标），查找不到返回-1
 #  str.rfind()  反向查找
 print(s.find("a",3))    # 7
 print(s.find("z",0))    # -1
 
-#9 str.index()  查找字符串中是否具有指定的字符串，返回第一次出现的位置，查找不到直接报错
+#9 str.index()  查找字符串中是否具有指定的字符串，返回第一次出现的位置（下标），查找不到直接报错
 #  str.rindex() 反向查找
 print(s.index("a",3))   # 7
 #print(s.index("z",0))  Error
